02d1384b7b4084734993edbf9ac770c631ffa1ca
Zn4rK <alexander@paxxmedia.se>
2023-11-26 15:20:53 +0100

    add check for css vars in hyphenate properties

--- packages/engine/src/helpers/hyphenateProperty.ts
+++ packages/engine/src/helpers/hyphenateProperty.ts
@@ -1,9 +1,15 @@
+// Modified from
 // https://github.com/styletron/styletron/blob/b552ddc5050a8cc5eec84a46a299d937d3bb0112/packages/styletron-engine-atomic/src/hyphenate-style-name.ts
 const uppercasePattern = /[A-Z]/g;
 const msPattern = /^ms-/;
+const cssVarPattern = /^--/;
 const cache = {};
 
 export function hyphenateProperty(property: string): string {
+  if (cssVarPattern.test(property)) {
+    return property;
+  }
+
   return property in cache
     ? cache[property]
     : (cache[property] = property
