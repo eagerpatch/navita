5f8ca95309c3cb0f12c80acb62d1729b106f9b22
Zn4rK <alexander@paxxmedia.se>
2024-10-01 12:33:31 +0200

    Removing unnecessary context for compiledFunction

--- packages/core/src/helpers/createCompiledFunction.ts
+++ packages/core/src/helpers/createCompiledFunction.ts
@@ -12,19 +12,9 @@ Object
   }), {});
 `).runInContext(context) as Record<string, unknown>;
 
-const vmGlobalsNonEnumerableRecord = {};
-
-Object.getOwnPropertyNames(global).forEach((name) => {
-  // The second condition here is to prevent jest's Symbol polyfill from being added.
-  // https://github.com/jestjs/jest/blob/25a8785584c9d54a05887001ee7f498d489a5441/packages/jest-util/src/installCommonGlobals.ts#L49
-  if (!vmGlobalsRecord[name] && global[name] !== globalThis.Symbol) {
-    vmGlobalsNonEnumerableRecord[name] = global[name];
-  }
-});
-
 export function createCompiledFunction<Return>(
   source: string,
-  define: (deps: string[], handlerFn: (...args) => void) => Return,
+  define: (deps: string[], handlerFn: (...args: any[]) => void) => Return,
 ) {
   // This looks a bit weird, but it's much quicker to do it this way than to
   // run vm.runInContext. There's a bit more information in this issue:
@@ -32,7 +22,6 @@ export function createCompiledFunction<Return>(
   const sandbox = {
     define,
     ...vmGlobalsRecord,
-    ...vmGlobalsNonEnumerableRecord,
     window: createMagicProxy(),
     document: createMagicProxy(),
     navigator: createMagicProxy(),
